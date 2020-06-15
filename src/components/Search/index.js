c8664a73894f22fcd2139860d3ca721f947aa279
asosunoff <al.sosunoff@gmail.com>
2020-06-15 23:27:11 +0300

    added search component and card component

---
+++ src/components/Search/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default () => {
+	return (
+		<div className="form-group">
+			<input
+				type="text"
+				className="form-control"
+				placeholder="Введите ник пользователя"
+			/>
+		</div>
+	);
+};
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React from "react";
+import Search from "../components/Search";
+import Card from "../components/Card";
 
 export default () => {
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    );
-}
+	const cards = new Array(15).fill("").map((_, i) => i);
+
+	return (
+		<>
+			<Search />
+
+			<div className="row">
+				{cards.map((card) => {
+					return (
+						<div className="col-sm-4 mb-4" key={card}>
+							<Card />
+						</div>
+					);
+				})}
+			</div>
+		</>
+	);
+};
