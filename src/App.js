815ef1e79958f0369944f721a15a4002dd081bc8
asosunoff <al.sosunoff@gmail.com>
2020-06-16 00:18:01 +0300

    added useState for search component

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
 			<BrowserRouter>
 				<Navbar />
 				<div className="container pt-4">
-					<Alert alert={{ text: "test" }} />
+					<Alert />
 					<Switch>
 						<Route path="/" exact component={Home} />
 						<Route path="/about" component={About} />
