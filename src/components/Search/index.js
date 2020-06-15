f8bb762c75d5ffcc7f36f31202468e3502e6f1a5
asosunoff <al.sosunoff@gmail.com>
2020-06-16 00:07:26 +0300

    added context and state by component alert

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AlertContext } from "../../context/Alert/context";
 
 export default () => {
+	const { show } = useContext(AlertContext);
+	const onSubmit = (event) => {
+		if (event.key === "Enter") {
+			show("Я есть Алерт");
+		}
+	};
+
 	return (
 		<div className="form-group">
 			<input
 				type="text"
 				className="form-control"
 				placeholder="Введите ник пользователя"
+				onKeyPress={onSubmit}
 			/>
 		</div>
 	);
--- src/context/Alert/state.js
+++ src/context/Alert/state.js
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { useReducer } from "react";
 import { AlertContext } from "./context";
+import { AlertReducer } from "./reducer";
+import { HIDE_ALERT, SHOW_ALERT } from "../types";
 
 export const AlertState = ({ children }) => {
-	return <AlertContext.Provider>{children}</AlertContext.Provider>;
+	const [state, dispatch] = useReducer(AlertReducer, null);
+
+	const hide = () => dispatch({ type: HIDE_ALERT });
+	const show = (text, type = "secondary") =>
+		dispatch({ type: SHOW_ALERT, payload: { type, text } });
+
+	return (
+		<AlertContext.Provider
+			value={{
+				hide,
+				show,
+				alert: state,
+			}}
+		>
+			{children}
+		</AlertContext.Provider>
+	);
 };
